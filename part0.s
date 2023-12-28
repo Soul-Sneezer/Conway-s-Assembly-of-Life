2be93a750c3c95504762a89a13e92072ec133297
Soul Sneezer <petrerobertcristian@gmail.com>
2023-12-28 02:28:47 +0200

    Ain't much but it's honest work.

--- part0.s
+++ part0.s
@@ -3,26 +3,24 @@ n: .space 1
 m: .space 1
 p: .space 1
 k: .space 1
-arguments: .space 256
-list: .space 400
+arguments: .space 1024
+matrix: .space 400
 .text
 .global main
 main:
+
 read_input:
 	mov $3, %eax
-	mov $0, %ebx
+	xor %ebx, %ebx
 	lea n, %ecx
-	mov $1, %edx
+	mov $1024, %edx
 	int $0x80
 
-	lea m, %ebx
-	int $0x80
-
-	lea p, %ebx
-	int $0x80
-	mov p, %esi
-
-read_arguments:
+	mov $2, %edi
+	movb (%ecx, %edi, 1), %al
+	sub $51, %eax
+	/*
+get_values:
 	cmp $0, %esi
 	je initialize_list
 	dec %esi
@@ -36,7 +34,7 @@ read_arguments:
 
 	lea k, %ebx
 	int $0x80
-initialize_list:
+initialize_matrix:
 	mov n, %eax
 	mul m
 	dec %eax
@@ -47,7 +45,10 @@ initialize_list:
 	je print_list
 	dec %ecx
 	jmp initialize_list
-print_list:
+
+execute_evolution:
+
+print_matrix:
 	dec %esi
 	mov $4, %eax
 	mov $1, %ebx
@@ -56,6 +57,7 @@ print_list:
 	int $0x80
 	cmp $0, %esi
 	jne print_list
+	*/
 end_program:
 	mov $1, %eax
 	xor %ebx, %ebx
